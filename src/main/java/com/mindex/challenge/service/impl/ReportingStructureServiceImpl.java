e224352b5739ef14d16f9c61ddaeca8a913bb07c
jschwabel <41974199+jschwabel@users.noreply.github.com>
2018-08-06 22:31:39 -0400

    clean up

--- src/main/java/com/mindex/challenge/service/impl/ReportingStructureServiceImpl.java
+++ src/main/java/com/mindex/challenge/service/impl/ReportingStructureServiceImpl.java
@@ -40,53 +40,21 @@ public class ReportingStructureServiceImpl implements ReportingStructureService 
         try {
             if(e!=null){
                 LOG.debug("1 e.id["+e.getEmployeeId()+"]");
-//                List<Employee> directReports = e.getDirectReports();
-//                if(directReports!=null) {
-//                    Employee eDR=null;
-//                    numDirReports=directReports.size();
-//                    LOG.debug("2 e.id["+e.getEmployeeId()+"] numDirReports["+numDirReports+"] numSubReports["+numSubReports+"] totalSubReports["+totalSubReports+"]");
-//                    
-//                    for(int i=0;i<directReports.size();++i) {
-//                        eDR = directReports.get(i);
-//                        if(eDR!=null) {
-//                            LOG.debug("3 e.id["+e.getEmployeeId()+"] eDR.id["+eDR.getEmployeeId()+"] eDR.lastName["+eDR.getLastName()+"] ["+i+"]"); 
-//                            numSubReports = calcNumberOfReports(eDR);
-//                            totalSubReports=totalSubReports+numSubReports;
-//                            LOG.debug("4 e.id["+e.getEmployeeId()+"] eDR.id["+eDR.getEmployeeId()+"] numDirReports["+numDirReports+"] numSubReports["+numSubReports+"] totalSubReports["+totalSubReports+"] ["+i+"]"); 
-//                        }
-//                    }   
-//                }
-//                    List<String> directReportIds=e.getDirectReports();
-//                    if(directReportIds!=null) {
-//                        Employee eDR=null;
-//                        numDirReports=directReportIds.size();
-//                        for(int i=0;i<directReportIds.size();++i) {
-//                            eDR = employeeRepository.findByEmployeeId(directReportIds.get(i));
-//                            if(eDR!=null) {
-//                                numSubReports = calcNumberOfReports(eDR);
-//                                totalSubReports=totalSubReports+numSubReports;
-//                                LOG.debug("4 e.id["+e.getEmployeeId()+"] eDR.id["+eDR.getEmployeeId()+"] eDR.lastName["+eDR.getLastName()+"] numDirReports["+numDirReports+"] numSubReports["+numSubReports+"] totalSubReports["+totalSubReports+"] ["+i+"]"); 
-//                            }
-//                        }
-//                    }
-
-                    List<Employee> directReports=e.getDirectReports();
-                    if(directReports!=null) {
-                        Employee eDR=null;
-                        numDirReports=directReports.size();
-                        for(int i=0;i<directReports.size();++i) {
-                            eDR = employeeService.read(directReports.get(i).getEmployeeId());
-                            if(eDR!=null) {
-                                LOG.debug("3 e.id["+e.getEmployeeId()+"] eDR.id["+eDR.getEmployeeId()+"] eDR.lastName["+eDR.getLastName()+"] numDirReports["+numDirReports+"] numSubReports["+numSubReports+"] totalSubReports["+totalSubReports+"] ["+i+"]"); 
-                                numSubReports = calcNumberOfReports(eDR);
-                                totalSubReports=totalSubReports+numSubReports;
-                                LOG.debug("4 e.id["+e.getEmployeeId()+"] eDR.id["+eDR.getEmployeeId()+"] eDR.lastName["+eDR.getLastName()+"] numDirReports["+numDirReports+"] numSubReports["+numSubReports+"] totalSubReports["+totalSubReports+"] ["+i+"]"); 
-                            }
+                List<Employee> directReports=e.getDirectReports();
+                if(directReports!=null) {
+                    Employee eDR=null;
+                    numDirReports=directReports.size();
+                    for(int i=0;i<directReports.size();++i) {
+                        eDR = employeeService.read(directReports.get(i).getEmployeeId());
+                        if(eDR!=null) {
+                            LOG.debug("3 e.id["+e.getEmployeeId()+"] eDR.id["+eDR.getEmployeeId()+"] eDR.lastName["+eDR.getLastName()+"] numDirReports["+numDirReports+"] numSubReports["+numSubReports+"] totalSubReports["+totalSubReports+"] ["+i+"]"); 
+                            numSubReports = calcNumberOfReports(eDR);
+                            totalSubReports=totalSubReports+numSubReports;
+                            LOG.debug("4 e.id["+e.getEmployeeId()+"] eDR.id["+eDR.getEmployeeId()+"] eDR.lastName["+eDR.getLastName()+"] numDirReports["+numDirReports+"] numSubReports["+numSubReports+"] totalSubReports["+totalSubReports+"] ["+i+"]"); 
                         }
                     }
-            
-                
-            LOG.debug("5 e.id["+e.getEmployeeId()+"] numDirReports["+numDirReports+"] numSubReports["+numSubReports+"] totalSubReports["+totalSubReports+"]");
+                }
+                LOG.debug("5 e.id["+e.getEmployeeId()+"] numDirReports["+numDirReports+"] numSubReports["+numSubReports+"] totalSubReports["+totalSubReports+"]");
             }
         } catch(Exception ex) {
             LOG.error(ex.getMessage());
